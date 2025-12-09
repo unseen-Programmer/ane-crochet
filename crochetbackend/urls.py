a1de8f9f9e21008a83df0fd5ec66f8b910cf89a8
unseen-Programmer <programmerunseen@gmail.com>
2025-12-09 20:10:10 +0530

    Fix settings.py crash and restore urls.py

--- crochetbackend/urls.py
+++ crochetbackend/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
     path("api/", include("shop.urls")),
 ]
 
-# ✅ THIS IS REQUIRED TO SERVE IMAGES
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
